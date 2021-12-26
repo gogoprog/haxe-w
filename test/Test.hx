c4e62c204f9189c672ecd7ea558b2078f46a5c46
Gauthier Billot <gogoprog@gmail.com>
2021-12-26 22:29:06 +0100

    More example

--- test/Test.hx
+++ test/Test.hx
@@ -1,11 +1,12 @@
-import w.W;
 import js.Browser.document;
+import w.W;
 
 function main() {
     var c:js.html.CanvasElement = cast document.querySelector("canvas");
     W.reset(c);
     W.camera({x:9, y:8, z:20, rx:-13, ry:15});
-    W.cube({x:5, w:2, h:.5, d:.5, b:"f44"});
-    W.sphere({x:0, size:2, b:"388"});
+    W.light({x:0.5, y:-1, z:-0.5});
+    W.cube({x:5, w:3, h:.5, d:.5, b:"f44"});
+    W.sphere({x:0, size:4, b:"388"});
     W.pyramid({x:-5, size:4, b:"909"});
 }
--- test/test.js
+++ test/test.js
@@ -3,8 +3,9 @@ function Test_main() {
 	var c = window.document.querySelector("canvas");
 	W.reset(c);
 	W.camera({ x : 9, y : 8, z : 20, rx : -13, ry : 15});
-	W.cube({ x : 5, w : 2, h : .5, d : .5, b : "f44"});
-	W.sphere({ x : 0, size : 2, b : "388"});
+	W.light({ x : 0.5, y : -1, z : -0.5});
+	W.cube({ x : 5, w : 3, h : .5, d : .5, b : "f44"});
+	W.sphere({ x : 0, size : 4, b : "388"});
 	W.pyramid({ x : -5, size : 4, b : "909"});
 }
 var haxe_iterators_ArrayIterator = function(array) {
