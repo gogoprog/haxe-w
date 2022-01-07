2355bb219420e22e9bf8a74e5e45f7c2c5cc6e54
Gauthier Billot <gogoprog@gmail.com>
2022-01-07 12:00:12 +0100

    Update

--- src/w/W.hx
+++ src/w/W.hx
@@ -37,24 +37,10 @@ typedef ObjectSettings = {
     @:optional var ns:Bool;
 };
 
-typedef MoveSettings = {
-    @:optional var n:String;
-    @:optional var size:Float;
-    @:optional var w:Float;
-    @:optional var h:Float;
-    @:optional var d:Float;
-    @:optional var x:Float;
-    @:optional var y:Float;
-    @:optional var z:Float;
-    @:optional var rx:Float;
-    @:optional var ry:Float;
-    @:optional var rz:Float;
-};
-
 typedef ModelData = {
     var vertices:Array<Float>;
     @:optional var indices:Array<Float>;
-    var uv:Array<Float>;
+    @:optional var uv:Array<Float>;
 };
 
 extern class W_ implements Dynamic {
@@ -71,7 +57,7 @@ extern class W_ implements Dynamic {
 
     public function add(name:String, data:ModelData):Void;
 
-    public function move(settings:MoveSettings, ?delay:Float):Void;
+    public function move(settings:ObjectSettings, ?delay:Float):Void;
     public function delete(name:String, ?delay:Float):Void;
 }
 
